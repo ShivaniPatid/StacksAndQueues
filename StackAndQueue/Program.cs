fadf1632e40d6dc54bcafd16f6a3f12eb13924a7
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-14 13:06:15 +0530

    [Shivani] UC1 - Push elements in the stack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -10,6 +10,23 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Chosse a option\n 1 . Push the elements in the stack");
+            int option=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            switch (option)
+            {
+                case 1:
+                    StackPushOperation stack1 = new StackPushOperation();
+                    stack1.Push(70);
+                    stack1.Push(30);
+                    stack1.Push(56);
+                    stack1.Display();
+                    break;
+                default:
+                    Console.WriteLine("Choose a appropriate option");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
